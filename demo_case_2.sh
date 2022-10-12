ca018f94fd96d887c60fd93f180b1a69d48a4d5d
Zachary Povey <zachary.povey@autotrader.co.uk>
2022-10-12 13:59:33 +0100

    Make scripts a touch more readable

--- demo_case_2.sh
+++ demo_case_2.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 COLOUR_RED='\033[1;31m'
 COLOUR_END='\033[0m'
-CORRECT_CRD_NAME=testthings.helmss.test
+VALID_CRD_NAME=testthings.helmss.test
+INVALID_CRD_NAME=invalid
 
 log () {
 	printf "${COLOUR_RED}${1}${COLOUR_END}\n\n"
@@ -10,11 +11,11 @@ log "Cleaning up any previous runs..."
 ./cleanup.sh
 log "Attempting initial install..."
 kubectl create namespace helm-ss-bug > /dev/null 2>&1
-helm install -n helm-ss-bug helm-ss-bug ./chart --set crd_name=$CORRECT_CRD_NAME > /dev/null 2>&1
+helm install -n helm-ss-bug helm-ss-bug ./chart --set crd_name=$VALID_CRD_NAME > /dev/null 2>&1
 log "Initial install succeeds:"
 helm history -n helm-ss-bug helm-ss-bug
 log "\nNow if we break the name and upgrade..."
-helm upgrade -n helm-ss-bug helm-ss-bug ./chart --set crd_name=invalid > /dev/null 2>&1
+helm upgrade -n helm-ss-bug helm-ss-bug ./chart --set crd_name=$INVALID_CRD_NAME > /dev/null 2>&1
 log "We can see that the upgrade failed:"
 helm history -n helm-ss-bug helm-ss-bug
 log "\nIf we then try to rollback to our last good release..."
